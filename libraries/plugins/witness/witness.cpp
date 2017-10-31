4d71262814617bb94c8634efbe6b8b0aed757a0a
toomany008 <182417746@qq.com>
2017-10-31 22:24:54 +0800

    change welcome

--- libraries/plugins/witness/witness.cpp
+++ libraries/plugins/witness/witness.cpp
@@ -45,9 +45,9 @@ void new_chain_banner( const graphene::chain::database& db )
       "********************************\n"
       "*                              *\n"
       "*   ------- NEW CHAIN ------   *\n"
-      "*   - Welcome to Graphene! -   *\n"
+      "*   - Welcome to FinChain! -   *\n"
       "*   ------------------------   *\n"
-      "*                              *\n"
+      "*   powered by Graphene        *\n"
       "********************************\n"
       "\n";
    if( db.get_slot_at_time( fc::time_point::now() ) > 200 )

ded15ab53145773ea8936938cdbdb818ce7c873a
daidai <jzcdj@163.com>
2017-11-08 15:13:46 +0800

    improve a little

--- libraries/chain/lock_balance_evaluator.cpp
+++ libraries/chain/lock_balance_evaluator.cpp
@@ -177,7 +177,7 @@ namespace graphene { namespace chain {
 			   
 			   d.adjust_balance(o.issuer, asset(item.initial_lock_balance, item.asset_id));
 
-			   if (itr->type == unlock_balance_operation::expire)
+			   if (itr->expired)
 			   {
 				   d.adjust_balance(o.issuer, asset(item.locked_balance,item.asset_id));
 			   }

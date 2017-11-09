1761ec8dbb318608dd2aabb19089edf0af401cc1
daidai <jzcdj@163.com>
2017-11-09 16:33:42 +0800

    register operation evaluator when init db

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -180,6 +180,8 @@ void database::initialize_evaluators()
    register_evaluator<blind_transfer_evaluator>();
    register_evaluator<asset_claim_fees_evaluator>();
    register_evaluator<lock_balance_evaluator>();
+   register_evaluator<set_lock_data_evaluator>();
+   register_evaluator<unlock_balance_evaluator>();
 }
 
 void database::initialize_indexes()

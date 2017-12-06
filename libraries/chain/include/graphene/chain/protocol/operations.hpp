665d0f908aec4a4217e5b8d183936a133347a500
toomany008 <182417746@qq.com>
2017-12-06 15:36:45 +0800

    chang test.json and add operation comment

--- libraries/chain/include/graphene/chain/protocol/operations.hpp
+++ libraries/chain/include/graphene/chain/protocol/operations.hpp
@@ -53,47 +53,47 @@ namespace graphene { namespace chain {
             limit_order_cancel_operation,
             call_order_update_operation,
             fill_order_operation,           // VIRTUAL
-            account_create_operation,
+            account_create_operation,                 //5
             account_update_operation,
             account_whitelist_operation,
             account_upgrade_operation,
             account_transfer_operation,
-            asset_create_operation,
+            asset_create_operation,                   //10
             asset_update_operation,
             asset_update_bitasset_operation,
             asset_update_feed_producers_operation,
             asset_issue_operation,
-            asset_reserve_operation,
+            asset_reserve_operation,                  //15
             asset_fund_fee_pool_operation,
             asset_settle_operation,
             asset_global_settle_operation,
             asset_publish_feed_operation,
-            witness_create_operation,
+            witness_create_operation,                 //20
             witness_update_operation,
             proposal_create_operation,
             proposal_update_operation,
             proposal_delete_operation,
-            withdraw_permission_create_operation,
+            withdraw_permission_create_operation,     //25
             withdraw_permission_update_operation,
             withdraw_permission_claim_operation,
             withdraw_permission_delete_operation,
             committee_member_create_operation,
-            committee_member_update_operation,
+            committee_member_update_operation,        //30
             committee_member_update_global_parameters_operation,
             vesting_balance_create_operation,
             vesting_balance_withdraw_operation,
             worker_create_operation,
-            custom_operation,
+            custom_operation,                         //35
             assert_operation,
             balance_claim_operation,
             override_transfer_operation,
             transfer_to_blind_operation,
-            blind_transfer_operation,
+            blind_transfer_operation,                 //40
             transfer_from_blind_operation,
             asset_settle_cancel_operation,  // VIRTUAL
             asset_claim_fees_operation,
             fba_distribute_operation,       // VIRTUAL
-            lock_balance_operation,
+            lock_balance_operation,                   //45
             set_lock_data_operation,
             unlock_balance_operation,
             donation_balance_operation
